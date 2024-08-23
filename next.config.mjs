b192e4b38acc65d891fa88dbfc865452b959d1a9
elkhamyyali <elkhamyyalimohamedali@gmail.com>
2024-08-23 21:30:37 +0300

    Initial commit with all files

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, // Set to `true` to enable React's strict mode during development
+  reactStrictMode: false,
   eslint: {
-    ignoreDuringBuilds: true, // Disable ESLint during production builds
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true, // Disable TypeScript errors during production builds
+    ignoreBuildErrors: true,
   },
-  // Set to `true` if you want to use `next export` for static site generation
-  // For most applications, this should be omitted or set to `false` unless you have a specific use case
-  output: "export",
+
+  // output: "export",
 
   images: {
-    unoptimized: true, // Set to `false` if you want Next.js's built-in image optimization
+    unoptimized: true,
   },
 };
 
