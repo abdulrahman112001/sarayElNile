f5389b2d042104a833019b3352f5c425667c875a
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-08-25 04:38:37 +0300

    updaets

--- src/pages/top-packages/index.tsx
+++ src/pages/top-packages/index.tsx
@@ -1,7 +1,9 @@
 import Laptop from "@/components/templates/laptop/Laptop";
 import Mobile from "@/components/templates/mobile/Mobile";
+import fetchData from "@/helper/FetchData";
 
-export default function Home() {
+export default function Home({toursData}) {
+  console.log("🚀 ~ Home ~ toursData:", toursData)
   return (
     <div className="md:p-6 p-0 bg-[#FAFAFA]">
       {/* Mobile view */}
@@ -15,4 +17,14 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const toursData = await fetchData("tours");
+
+  return {
+    props: {
+      toursData,
+    },
+  };
 }
